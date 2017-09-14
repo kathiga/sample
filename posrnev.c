d2a525f60d3f9f13977684f30cacaaa14689144d
kathiga <30829951+kathiga@users.noreply.github.com>
2017-09-14 11:24:51 +0530

    Create posrnev.c

---
+++ posrnev.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf(" enter the number")
+scanf("%d",&n);
+if(n<0)
+printf("given num is negative");
+else
+printf("given number is positive");
+}
