1ed7b32b641e1ed9d1dbc1b43a1b79c5fb79bb0d
kathiga <30829951+kathiga@users.noreply.github.com>
2017-09-17 12:04:23 +0530

    Create palindrom.c

---
+++ palindrom.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int num,rev=0,d;
+printf("enter the number");
+scanf("%d"&num);
+if(n!=0)
+{
+d=num%10;
+rev=rev*10+d;
+num=num/10;
+}
+elseif(n==rev)
+printf("given number is palindrom");
+else
+printf("not palindrom");
+}
