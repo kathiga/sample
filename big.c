93f95f9de156255c1599a481c8c79c9dce252829
kathiga <30829951+kathiga@users.noreply.github.com>
2017-09-17 10:16:30 +0530

    Create big.c

---
+++ big.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the value");
+scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c))
+printf("a is biggest number");
+elseif(b>c)
+printf(" b is biggest number");
+else
+printf(" c is biggest number");
+]
