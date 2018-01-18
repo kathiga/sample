1777406f8c05d29b03f3e51f6368b66e3040c72c
kathiga <30829951+kathiga@users.noreply.github.com>
2018-01-18 16:37:01 +0530

    Rename sumofdigit.c to sum of digit.c

---
+++ sum of digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+	int n,m,c=0;
+	printf("enter the num");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		m=n%10;
+		c=c+m;
+		n=n/10;
+	}
+	printf("\n sum of the num is :%d",c);
+}
