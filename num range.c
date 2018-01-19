1d6644124405cb49b1f5d8518b8b5f5beb961abe
kathiga <30829951+kathiga@users.noreply.github.com>
2018-01-19 08:38:44 +0000

    Create num range.c

---
+++ num range.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+int n;
+printf("enter the num between 1-10");
+scanf("%d",&n);
+if(n<=10)
+printf("\n the number is from the range 1-10");
+else
+printf("\n enter the number within range");
+}
