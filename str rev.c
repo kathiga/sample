f8bcb22de4baffdcd08192dd5a3d1daa16cdac6d
kathiga <30829951+kathiga@users.noreply.github.com>
2018-01-19 08:15:34 +0000

    Update str rev.c

--- str rev.c
+++ str rev.c
@@ -3,9 +3,9 @@
 int main()
 {
 char a[789];
-printf("enter the tring");
+printf("enter the string");
 gets(a);
 strrev(a);
-printf("the revered sting is",a);
+printf("the revered string is",a);
 return 0;
 }
