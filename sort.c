4f9a89a3803c163d8c23c53feddff9d009a945f0
kathiga <30829951+kathiga@users.noreply.github.com>
2018-01-21 11:50:05 +0530

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+int a[100],i,j,n,temp;
+printf("enter the size of array");
+scanf("%d",&n);
+printf("\n enter the array value");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+temp=a[j];
+}
+}
+}
+for(i=0;i<n;i++)
+{
+printf(" \n the sorted array is %d",a[i]);
+}
+return (0);
+}
