490f37f1102d2bd8e06f2bb19e63d6dff1d9f6d0
kathiga <30829951+kathiga@users.noreply.github.com>
2018-01-21 09:44:28 +0530

    Create neareven.c

---
+++ neareven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter the num");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("tne nearest even number is %d",n);
+	}
+	else
+	{
+		printf("\n tne nearest even number is %d",n-1);
+}
+}
