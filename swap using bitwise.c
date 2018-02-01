9a067d15dc69e06ee1cde4f35e42d9b74611433b
kathiga <30829951+kathiga@users.noreply.github.com>
2018-02-01 07:58:05 +0000

    Create swap using bitwise.c

---
+++ swap using bitwise.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  int a,b;
+  printf("enter a and b value");
+  scanf("%d%d",&a,&b);
+  a^=b^=a^=b;
+  printf("\n SWAP USING BITWISE OPERATOR");
+  printf("\n swap value is %d and %d",a,b);
+  return 0;
+}
