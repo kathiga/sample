8c880439d1a4f8f4999d3ba33ffda47dcace7a9a
kathiga <30829951+kathiga@users.noreply.github.com>
2018-02-02 15:15:43 +0530

    Create btn alter.c

---
+++ btn alter.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,b;
+    printf("enter the number");
+    scanf("%d",&n);
+    printf("enter the starting and ending point");
+    scanf("%d%d",&a,&b);
+    for(i=a;i<b;i++)
+    {
+        
+    }
+}
