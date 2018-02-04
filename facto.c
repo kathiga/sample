53e16769f9695f269769a3c05b6430c9c6d86fcb
kathiga <30829951+kathiga@users.noreply.github.com>
2018-02-04 15:54:02 +0530

    Create facto.c

---
+++ facto.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,f=1,i;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        f=f*i;
+    }
+    printf("fact is %d",f);
+}
