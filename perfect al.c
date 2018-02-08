e102e5cc9e2bd52452ae0738f6511da249862835
kathiga <30829951+kathiga@users.noreply.github.com>
2018-02-08 11:30:39 +0000

    Create perfect al.c

---
+++ perfect al.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  int a,b,i,n;
+  printf("enter two numbers");
+  scanf("%d%d",&a,&b);
+  n=a*b;
+  for(i=1;i=<n;i++)
+  {
+      if(i*i==n)
+      p
+  }
+}
