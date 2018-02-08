e61e12a09953393094d3bbbfbcb663f9086e5bc9
kathiga <30829951+kathiga@users.noreply.github.com>
2018-02-08 11:10:13 +0000

    Create float into int.c

---
+++ float into int.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float n;
+    int a;
+    printf("enter the float value ");
+    scanf("%f",&n);
+    n++;
+    a=n;
+    printf("\n tne roundup value is %d",a);
+}
