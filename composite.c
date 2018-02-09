0638d31dc46e9b8e3c51e79ef9dad80d9704627f
kathiga <30829951+kathiga@users.noreply.github.com>
2018-02-09 14:24:30 +0530

    Create composite.c

---
+++ composite.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,i,count=0;
+printf("enter n value");
+scanf("%d",&n);
+for(i=2;i<n;i++)
+{
+if((n%i)==0)
+count++;
+}
+if(count==0)
+printf("not a composite");
+else
+printf("composite number");
+}
