1d392dc14e100e4a5ee65f5cd579ab930734fe50
kathiga <30829951+kathiga@users.noreply.github.com>
2018-02-09 14:50:41 +0530

    Update perfect al.c

--- perfect al.c
+++ perfect al.c
@@ -2,13 +2,17 @@
 #include<string.h>
 int main()
 {
-  int a,b,i,n;
+  int a,b,i,n,count=0;
   printf("enter two numbers");
   scanf("%d%d",&a,&b);
   n=a*b;
-  for(i=1;i=<n;i++)
+  for(i=1;i<=n;i++)
   {
       if(i*i==n)
-      p
+      count++;
   }
+  if(count>=1)
+  printf("perfect");
+  else
+  printf("not a perfect");
 }
