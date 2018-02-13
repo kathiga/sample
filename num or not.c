47604fd6163841ca7e7136734e11003b59ce8668
kathiga <30829951+kathiga@users.noreply.github.com>
2018-02-13 16:46:31 +0530

    Create num or not.c

---
+++ num or not.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char a[10],i;
+    printf("enter the number");
+    scanf("%s",&a);
+    if(a[i]>='0'&&a[i]<='9')
+    printf("\n yes");
+    else
+    printf("\n no");
+    return 0;
+}
