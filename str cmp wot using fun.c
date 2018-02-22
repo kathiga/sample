95ccdedf9ff8f22c9d2960acba2e423b43255ee1
kathiga <30829951+kathiga@users.noreply.github.com>
2018-02-22 16:41:57 +0530

    Create str cmp wot using fun.c

---
+++ str cmp wot using fun.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[10],b[10];
+int i,j,s1,s2,count=0;
+printf("enter two strings");
+gets(a);
+gets(b);
+for(i=0;a[i]!='\0';i++)
+{
+   for(j=0;b[j]!='\0';j++)
+   {
+        if(a[i]==b[j])
+        count++;
+   }
+}
+s1=strlen(a);
+s2=strlen(b);
+if(count==s1&&count==s2)
+printf("same");
+else
+printf("differ");
+if(s1>s2)
+printf("%s",a);
+else
+printf("%s",b);
+}
