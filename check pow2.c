81645d7492ae410beb166730732847f029444f63
kathiga <30829951+kathiga@users.noreply.github.com>
2018-02-27 16:38:28 +0530

    Update check pow2.c

--- check pow2.c
+++ check pow2.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 int main(void) 
 {
-	int n,s,i,sum=0,count=0;
-	printf("enter the num");
-	scanf("%d",&n);
-   for(i=1;i<n;i++)
+int n,i,sum=2,count=0;
+printf("enter the num");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
    {
-        sum=i*2;
-        printf("\nsum:%d",sum);
-        s=sum*2;
-        printf("s:%d",s);
-    /*if(n==sum)
-   count++;
-   sum=0;*/
+    sum=sum*2;
+    if(n==sum)
+    count++;
+   
     }
-    if(count>0)
-    printf("\n yes");
+if(count>0)
+printf("\n yes");
+else
+printf("\n no");
 }
