70e02e0e9c1ae04c56fd6a569288924150dfd5b2
kathiga <30829951+kathiga@users.noreply.github.com>
2018-02-27 15:56:04 +0530

    Create lowest n highest num.c

---
+++ lowest n highest num.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+int a[100],i,j,n,temp;
+printf("enter the size of array");
+scanf("%d",&n);
+printf("\n enter the array value");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+if(a[i]<a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+for(i=0;i<n;i++)
+{
+printf(" \t%d",a[i]);
+}
+printf("\nlowest num is %d and highest num is %d",a[0],a[n-1]);
+return (0);
+}
