ae4032c0718a4e3175e6d400acef17bdf99db5b6
kathiga <30829951+kathiga@users.noreply.github.com>
2018-03-01 17:00:00 +0530

    Update sumn.c

--- sumn.c
+++ sumn.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-int main(void) {
-	char a[100];
-	int sum=0,i,n;
-  printf(" enter n value");
-  scanf("%d",&n);
+int main() 
+{
+	int a[100],sum=0,i,n;
+	printf(" enter n value");
+        scanf("%d",&n);
 	for(i=1;i<=n;i++)
 	{
-	sum=sum+i;
+	    scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	sum=sum+a[i];
 	}
 	printf(" the sum of n number is %s",sum );
 }
