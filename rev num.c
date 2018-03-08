166cd80c830bd92b61b1e92a92fabc6275b76983
kathiga <30829951+kathiga@users.noreply.github.com>
2018-03-08 16:31:55 +0530

    Update rev num.c

--- rev num.c
+++ rev num.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
-int main()
+void main()
 {
-int n,d,rev=0;
-printf("enter the num");
-scanf("%d",&n);
-while(n!=0)
-{
-d=n%10;
-rev=(rev*10)+d;
-n=n/10;
-}
-  printf("rev num is %d",rev);
-return 0;
+    int n,d,num=0;
+    printf("enter the number");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        d=n%10;
+        num=d+(num*10);
+        n=n/10;
+    }
+    printf("%d",num);
 }
