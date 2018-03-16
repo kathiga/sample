387e20b871328addce99dada13211e7ebdc0b4ed
kathiga <30829951+kathiga@users.noreply.github.com>
2018-03-16 14:27:22 +0530

    Update no of words alter.c

--- no of words alter.c
+++ no of words alter.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
 char a[50];
-int count=0,i,word;
-printf("enter the word");
-scanf("%s",a);
-if(word==" ")
+int count=0,i,l;
+printf("enter the sentance");
+gets(a);
+l=strlen(a);
+for(i=0;i<l;i++)
+{
+if(a[i]==' ')
 {
 count++;
 }
-printf(" %d",count);
+}
+printf(" %d",count+1);
 return 0;
 }
